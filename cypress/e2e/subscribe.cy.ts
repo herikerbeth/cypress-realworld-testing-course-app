0f72710ebdd092182824b544805776bcda94effb
herikerbeth <herik.docarmo@hotmail.com>
2024-07-12 20:04:01 -0300

    verify if the email is displayed on the success screen

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -6,5 +6,6 @@ describe("Newsletter Subscribe Form", () => {
     it("allows users to subscribe to the email list", () => {
         cy.getByData("email-input").type("tom@aol.com")
         cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains("tom@aol.com")
     })
 })
