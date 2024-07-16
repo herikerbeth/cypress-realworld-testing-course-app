33445d3b171a0aa5e189eb5b33d73b054205f3ae
herikerbeth <herik.docarmo@hotmail.com>
2024-07-16 15:32:23 -0300

    practice: two additional tests, one for each course on the home page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -22,5 +22,15 @@ describe("home page", () => {
       cy.getByData("course-0").find("a").contains("Get started").click()
       cy.location("pathname").should("equal", "/testing-your-first-application")
     })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
